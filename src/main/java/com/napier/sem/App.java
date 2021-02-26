b30cb93eb28a2240189dd766b198db1a2fecbb08
Scott <scottydarroch@gmail.com>
2021-02-26 12:51:24 +0000

    Added report

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -12,14 +12,20 @@ public class App
 
         // Connect to database
         a.connect();
-        // Extract country population information
+        // Extract country population information from world.sql
         ArrayList<Country> countries = a.getAllPopulations();
-        //Send message to user explaining what the following report shows
+        //Send message to user explaining what the report shows
         System.out.println("A report showing all countries in the world in order of largest to smallest population.");
         //Print country population information
         a.printPopulations(countries);
-        // Test the size of the returned data - should be 239
-        System.out.println(countries.size());
+        //
+        // Extract population information from a region (Caribbean)
+        countries = a.getPopulationRegion();
+        // Output to user for clarity
+        System.out.println("A report showing the population of the Caribbean in order of largest to smallest.");
+        // printPopulations method can be reused here as it supplies the same output requirement as previous report
+        a.printPopulations(countries);
+        System.out.println("End of Reports.");
         // Disconnect from database
         a.disconnect();
     }
@@ -136,7 +142,7 @@ public class App
     public void printPopulations(ArrayList<Country> countries)
     {
         // Print header
-        System.out.println(String.format("%-5s %-48s %-20s %-15s %-15s %-15s", "Code", "Country Name", "Continent", "Region", "Population", "Capitol"));
+        System.out.println(String.format("%-5s %-48s %-20s %-30s %-15s %-15s", "Code", "Country Name", "Continent", "Region", "Population", "Capitol"));
         // Loop over all countries in the list
         for (Country coun : countries)
         {
@@ -144,6 +150,48 @@ public class App
                     String.format("%-5s %-48s %-20s %-30s %-15s %-15s",
                             coun.code, coun.name, coun.continent, coun.region, coun.population, coun.capital);
             System.out.println(emp_string);
+        }
+    }
+
+
+    /**
+     * Gets all the current population information.
+     * @return A list of population for a region (Caribbean), or null if there is an error.
+     */
+    public ArrayList<Country> getPopulationRegion()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT country.code, country.name, country.continent, country.region, country.population, country.capital "
+                            + "FROM country "
+                            + "WHERE country.region = 'Caribbean' "
+                            + "ORDER BY country.population DESC ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract country information
+            ArrayList<Country> countries = new ArrayList<Country>();
+            while (rset.next())
+            {
+                Country coun = new Country();
+                coun.code = rset.getString("country.code");
+                coun.name = rset.getString("country.name");
+                coun.continent = rset.getString("country.continent");
+                coun.region = rset.getString("country.region");
+                coun.population = rset.getInt("country.population");
+                coun.capital = rset.getString("country.capital");
+                countries.add(coun);
+            }
+            return countries;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population details");
+            return null;
         }
     }
 }
