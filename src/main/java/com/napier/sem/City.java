1022d81b25744662cdcfd3cf04166b3b829acd6d
Adam-Riddell <40431677@live.napier.ac.uk>
2021-03-02 15:25:45 +0000

    Added toString Function

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -31,4 +31,7 @@ public class City {
      */
     public int population;
 
+    public String toString() {
+        return String.format("%-5s %-48s %-20s %-30s %-15s", this.ID, this.name, this.country, this.district, this.population);
+    }
 }
