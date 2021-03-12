3eccb70167d74120b2620829bf6031215f100ea1
euanholmes <40454766@live.napier.ac.uk>
2021-03-12 13:59:51 +0000

    Added Test folder and example unit test class.

---
+++ src/test/java/UnitTest1.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitTest1
+{
+    @Test
+    void UnitTest1()
+    {
+        assertEquals(5,5);
+    }
+}
