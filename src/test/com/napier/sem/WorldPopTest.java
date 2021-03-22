32dc23ba24305b63fdc3b3bab1dbd0258cf853f9
Scott <scottydarroch@gmail.com>
2021-03-22 16:35:38 +0000

    More tersting

--- src/test/com/napier/sem/WorldPopTest.java
+++ src/test/com/napier/sem/WorldPopTest.java
@@ -1,31 +1,25 @@
 package com.napier.sem;
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public class WorldPopTest {
 
-    static App app = new App();
+    static App app;
 
-    public WorldPopTest() throws SQLException {
-    }
     @BeforeAll
     static void init()
     {
+        app = new App();
+        app.connect();
     }
 
     @Test
     void testGetWorldPopulation()
     {
-        app.connect();
-
-        Long testPop = 6078749450L;
-        Long worldPop = app.report26();
+        long testPop = 6078749450L;
+        long worldPop = app.report26();
         Assertions.assertEquals(worldPop, testPop, 1);
     }
 }
