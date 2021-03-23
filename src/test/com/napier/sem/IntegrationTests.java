fbcc56bf83f23ff004a996707da276ea8719fc5c
Adam-Riddell <40431677@live.napier.ac.uk>
2021-03-23 13:20:48 +0000

    Added integrationtests class

--- src/test/com/napier/sem/IntegrationTests.java
+++ src/test/com/napier/sem/IntegrationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class WorldPopTest {
+public class IntegrationTests {
 
     static App app;
 
