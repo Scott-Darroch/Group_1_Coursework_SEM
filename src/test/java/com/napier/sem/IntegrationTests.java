cc1da998dfc941ccf7f1490020eaf2855bff413d
Scott <scottydarroch@gmail.com>
2021-03-23 15:50:08 +0000

    New integration test added.

--- src/test/java/com/napier/sem/IntegrationTests.java
+++ src/test/java/com/napier/sem/IntegrationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
+ *
  * Class that contain all code for conducting integration tests for the project.
  * @author Euan Holmes,
  * @author Adam Riddell,
